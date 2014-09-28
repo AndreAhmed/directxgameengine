d0da0c9ce0d9811af329fbcd36679fb8d6ecc0fc
ahmed.coeia@gmail.com <ahmed.coeia@gmail.com@4f2c8530-292e-3638-f36f-e5faddcf3565>
2014-09-28 13:47:46 +0000

    Filtered VS Solution to library and Several project specific demos

---
+++ trunk/demos/Flappybird/GameApp.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Graphics.h"
+#include "SpriteBatch.h"
+#include "AnimatedSprite.h"
+#include "RenderStateHelper.h"
+#include "SpriteFont.h"
+#include "Timer.h"
+#include "Grid.h"
+#include "Camera.h"
+
+class GameApp
+{
+public:
+	GameApp();
+	~GameApp();
+	void Game_Init(HWND handle);
+	void Game_Render();
+	void Game_Update();
+	void Game_CleanUp();
+	void Graphics_2D();
+	void OnMouseDown(WPARAM btnState, int x, int y, HWND handle);
+	void OnMouseUp(WPARAM btnState, int x, int y);
+	void OnMouseMove(WPARAM btnState, int x, int y);
+	float Clamp(float x, float low, float high);
+public:
+	cGraphics m_Graphics;
+	cCamera   m_Camera;
+	cGrid     m_Grid;
+	cTimer	  m_Timer;
+	std::shared_ptr<cAnimatedSprite> m_Animated;
+	std::shared_ptr<DirectX::SpriteFont> m_SpriteFont;
+	RenderStateHelper* m_RenderStateHelper;
+ 
+	POINT mLastMousePos;
+	
+	
+};
